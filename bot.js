4756eda59f55336994bd61f7d0bcdf7e3886058c
zRazbliuto <42890358+zRazbliuto@users.noreply.github.com>
2018-09-01 16:54:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = -
+const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+client.user.setGame(` #Bُubbles Tumblr,2kk .. `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
