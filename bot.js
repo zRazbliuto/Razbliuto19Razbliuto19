ee9e11791314338ad158bb68e149abaa9f2369b4
zRazbliuto <42890358+zRazbliuto@users.noreply.github.com>
2018-09-04 07:56:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,5 +29,27 @@ client.user.setGame(` #Music is life ✨. `,"http://twitch.tv/S-F")
 });
 
 
+client.on('message', message => {
+if (message.content.startsWith("kick")) {
+    var mention = message.mentions.members.first();
+    if(!mention) return message.channel.send("يجب منشن العضو");
+
+    mention.kick("By: " + message.author.tag);
+    
+    message.channel.send("تم أعطاء كيك الى : " + mention.tag);
+};
+});
+
+client.on('message', message => {
+if (message.content.startsWith("ban")) {
+    var mention = message.mentions.members.first();
+    if(!mention) return message.channel.send("يجب منشن العضو");
+
+    mention.ban("By: " + message.author.tag);
+    
+    message.channel.send("تم أعطاء باند الى : " + mention.tag);
+};
+});
+
 
 client.login(process.env.BOT_TOKEN);
