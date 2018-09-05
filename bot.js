484b672184cf1100442cd0b5d0de5166e0f70dd1
zRazbliuto <42890358+zRazbliuto@users.noreply.github.com>
2018-09-05 12:38:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,50 +45,6 @@ client.on('message', message => {
     }
 });
 
-client.on("message", async message => {
-if(message.channel.type === "dm") return;
- if(message.content === (prefix . "B-Time")) { /// حط اي كلمة تبيها
- if (!message.channel.guild) return message.reply('**هذا الامر للسيرفرات فقط**');
-    let uptime = client.uptime;
-
-    let days = 0;
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    let notCompleted = true;
-
-    while (notCompleted) {
-
-        if (uptime >= 8.64e+7) { ///لا تعدل اي شي 
-
-            days++;
-            uptime -= 8.64e+7;
-
-        } else if (uptime >= 3.6e+6) {
-
-            hours++;
-            uptime -= 3.6e+6;
-
-        } else if (uptime >= 60000) {
-
-            minutes++;
-            uptime -= 60000;
-
-        } else if (uptime >= 1000) {
-            seconds++;
-            uptime -= 1000;
-
-        }
-
-        if (uptime < 1000)  notCompleted = false;
-
-    }
-
-    message.channel.send("`" + `${days} days, ${hours} hrs, ${minutes} min , ${seconds} sec` + "`");
-
-
-}
- });
 
 
 
