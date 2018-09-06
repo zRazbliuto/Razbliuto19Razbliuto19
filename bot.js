e0e3f344e36f97d7148a46d1dc237b5bb3ca3dbc
zRazbliuto <42890358+zRazbliuto@users.noreply.github.com>
2018-09-06 07:33:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,6 +46,34 @@ client.on('message', message => {
 });
 
 
+const Discord = require('discord.js');
+const client = new Discord.Client({disableEveryone: true})
+const Music = require('discord.js-musicbot-addon');
+client.login(`BOT_TOKEN `)
+client.on('ready', ()=> {
+    console.log('Ready!')
+})
+ Music.start(client, {
+    prefix: ".", // البرفكس هنا
+    youtubeKey: 'AIzaSyDIv8O9OwZdoTaghK3_SQmyGcQFzkXMW6E', //هنا ال yt v3 api key
+    global: false, // يكون للسيرفرات ؟ خليه false, يكون queue مشترك خليه true            
+    maxQueueSize: 50, // اقصى حجم لل queue        
+    playCmd: 'play', // امر التشغيل
+    volumeCmd: 'vol',     // امر الصوت
+    thumbnailType: 'high',
+    leaveCmd: 'stop',    // امر المغادرة
+    anyoneCanSkip: true, // تبي الكل يقدر يسوي skip? true > yep, false > nope
+    disableLoop: false, // تبي يكون فيه اعادة؟ , true > yep, false > nope
+    searchCmd: 'search', //امر البحث 
+    requesterName: true, 
+    inlineEmbeds: false,     
+    queueCmd: 'queue', // امر القائمة
+    pauseCmd: 'pause', // امر الايقاف المؤقت
+    resumeCmd: 'resume', // امر الاستمرار
+    skipCmd: 'skip', // امر السكب
+    loopCmd: 'loop', // امر الاعادة
+    enableQueueStat: true,
+  });
 
 
 client.login(process.env.BOT_TOKEN);
