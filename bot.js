5a3468db701d3d3667f1fdbde8acb8e87a63c211
zRazbliuto <42890358+zRazbliuto@users.noreply.github.com>
2018-12-10 15:35:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 const devs = ['490659461137039373'];
 
-const adminprefix = "-";
+const adminprefix = "#";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
